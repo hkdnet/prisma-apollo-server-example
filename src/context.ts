6c400ef106ddea65444e0fe0bb429e950dd4c185
hkdnet <satoko.itse@gmail.com>
2023-03-27 11:17:23 +0900

    Ensure the user exists

--- src/context.ts
+++ src/context.ts
@@ -4,17 +4,20 @@ import { decodeAuthHeader } from "./util/auth";
 
 export const prisma = new PrismaClient();
 
-export interface Context {
+export type Context = {
   prisma: PrismaClient;
   userId?: number;
-}
+};
 
-export const context = ({ req }: { req: Request }): Context => {
+export const context = ({ req }: { req: Request }): Promise<Context> => {
   const authHeader = req?.headers?.authorization;
   if (authHeader) {
     const { userId } = decodeAuthHeader(authHeader);
-    return { prisma, userId };
+
+    return prisma.user.findUnique({ where: { id: userId } }).then((user) => {
+      return { prisma, userId };
+    });
   } else {
-    return { prisma }; // TODO: 401?
+    return Promise.resolve({ prisma }); // TODO: 401?
   }
 };
